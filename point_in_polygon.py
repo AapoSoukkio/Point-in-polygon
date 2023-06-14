c981c7d2dfc13db4d055a60249b3094a320f3e65
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-06-14 12:12:22 +0300

    Generate the polygon with polygenerator module

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -1,2 +1,10 @@
 from polygenerator import random_polygon
 import matplotlib.pyplot as plt
+
+
+polygon = random_polygon(num_points=20)
+polygon.append(polygon[0])
+xs, ys = zip(*polygon)
+plt.plot(xs, ys, "b-", linewidth=0.8)
+plt.show()
+
