4a407e6f2da7b1e58ebf7a0b94e67c5d00dbc752
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-06-14 12:52:41 +0300

    Added onclick drawing

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -1,11 +1,21 @@
 from polygenerator import random_polygon
 import matplotlib.pyplot as plt
 
+def onclick(event):
+    xp, yp = event.xdata, event.ydata
+    plt.plot(xp, yp, "go", markersize=5)
+    plt.gcf().canvas.draw()
+
 
 polygon = random_polygon(num_points=20)
 polygon.append(polygon[0])
+edge_pairs = list(zip(polygon, polygon[1:] + polygon[:1]))
+plt.figure(figsize=(10, 10))
+plt.gca().set_aspect("equal")
 x_coordinates, y_coordinates = zip(*polygon)
+plt.gcf().canvas.mpl_connect('button_press_event', onclick)
 plt.plot(x_coordinates, y_coordinates, color="black", linestyle="-", linewidth=0.8)
 plt.show()
 
-edge_pairs = list(zip(polygon, polygon[1:] + polygon[:1]))
+
+
